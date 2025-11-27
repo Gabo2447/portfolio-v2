d6f9fe7b11849d105dde5d7bf01ec93d36013944
Gabo2447 <gabrieldeolival2@gmail.com>
2025-11-26 23:45:59 -0400

    fix(projects): Update Props interface to allow optional className and adjust section styling

--- src/components/sections.tsx
+++ src/components/sections.tsx
@@ -2,19 +2,22 @@ type Title = { icon: React.JSX.Element; name: string };
 
 interface Props {
   title?: Title;
-  id: `#${string}`;
+  id: string;
   children: React.ReactNode;
+  className?: string;
 }
 
-const Projects: React.FC<Props> = ({ title, id, children }) => {
+const Projects: React.FC<Props> = ({ title, id, children, className }) => {
   const sectionTitle = (
-    <h2 className="mb-5 flex gap-x-2 place-items-center font-Onest font-extrabold text-blue-100 text-2xl md:text-3xl lg:text-4xl">
+    <h2
+      className={`mb-5 flex gap-x-2 place-items-center font-Onest font-extrabold text-blue-100 text-2xl md:text-3xl lg:text-4xl`}
+    >
       {title?.name ? title.icon : ""}
       {title?.name ? title.name : ""}
     </h2>
   );
   return (
-    <section className="max-w-[1000px] mx-auto p-4" id={id}>
+    <section className={`max-w-[1000px] mx-auto p-4 ${className}`} id={id}>
       {sectionTitle}
       {children}
     </section>
