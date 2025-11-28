2160d9aa176dcebb9b34181732ea73f6fed6521c
Gabo2447 <gabrieldeolival2@gmail.com>
2025-11-27 20:02:23 -0400

    fix(projects): Update section title rendering and adjust section margin for consistency fix(data): Correct description text for clarity fix(styles): Change hover color to cyan for better visibility

--- src/components/sections.tsx
+++ src/components/sections.tsx
@@ -12,12 +12,13 @@ const Projects: React.FC<Props> = ({ title, id, children, className }) => {
     <h2
       className={`mb-5 flex gap-x-2 place-items-center font-Onest font-extrabold text-blue-100 text-2xl md:text-3xl lg:text-4xl`}
     >
-      {title?.name ? title.icon : ""}
-      {title?.name ? title.name : ""}
+      {title?.icon ?? ""}
+      {title?.name ?? ""}
     </h2>
   );
+
   return (
-    <section className={`max-w-[1000px] mx-auto p-4 ${className}`} id={id}>
+    <section className={`max-w-[1000px] mx-auto p-4 my-6 ${className}`} id={id}>
       {sectionTitle}
       {children}
     </section>
