4edc74babd9d1ad6af52667fab2dc6463d751dfd
Gabo2447 <gabrieldeolival2@gmail.com>
2025-12-01 23:18:37 -0400

    fix(types): Add type declaration for @fontsource-variable/onest

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "@/App.tsx";
+import "@fontsource-variable/onest";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
